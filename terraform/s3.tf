a1d45d09b8829f4c6e4ce311e2e3fa679a4388e8
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-15 17:13:31 +0200

    feat(secrets): Use S3 encrypted files for secrets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,10 +1,10 @@
 // TODO: I am keeping the config, since we will need them... it really depens on how we get the password for luks
-resource "aws_s3_bucket" "config" {
-  bucket = "${var.project_name}-config"
+resource "aws_s3_bucket" "secrets" {
+  bucket = "${var.project_name}-secrets"
 }
 
-resource "aws_s3_bucket_versioning" "config_versioning" {
-  bucket = aws_s3_bucket.config.id
+resource "aws_s3_bucket_versioning" "secrets_versioning" {
+  bucket = aws_s3_bucket.secrets.id
 
   versioning_configuration {
     status = "Enabled"
