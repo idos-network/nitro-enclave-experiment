c1bf828a85844481a260a47f3a7e4b806c670a8b
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-15 17:13:31 +0200

    feat(secrets): Use S3 encrypted files for secrets

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -56,28 +56,31 @@ resource "aws_iam_role_policy" "docdb_access" {
   })
 }
 
-# resource "aws_iam_role_policy" "kms_gocrypt_access" {
-#   name = "AllowKMSAccess"
-#   role = aws_iam_role.enclave_instance_role.id
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = [
-#           "kms:Encrypt",
-#           "kms:Decrypt",
-#           "kms:DescribeKey",
-#           "kms:GenerateDataKey*"
-#         ],
-#         Resource = aws_kms_key.gocryptfs.arn
-#       }
-#     ]
-#   })
-# }
-
 # Instance profile to attach the role to the EC2 instance
 resource "aws_iam_instance_profile" "enclave_instance_profile" {
   name = "${var.project_name}-instance-profile"
   role = aws_iam_role.enclave_instance_role.name
+}
+
+# Policy to read and write objects in the S3 bucket for secrets
+resource "aws_iam_role_policy" "s3_access" {
+  name = "AllowS3Access"
+  role = aws_iam_role.enclave_instance_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:GetObjectVersion",
+        ],
+        Resource = [
+          aws_s3_bucket.secrets.arn,
+          "${aws_s3_bucket.secrets.arn}/*",
+        ]
+      }
+    ]
+  })
 }
--- terraform/kms.tf
+++ terraform/kms.tf
@@ -1,6 +1,10 @@
-# KMS key for use with Nitro Enclaves (e.g., for attestation and secret decryption)
 resource "aws_kms_key" "enclave_instance_root_volume" {
   description = "Key for Nitro Enclave attestation demo"
+}
+
+resource "aws_kms_alias" "enclave_instance_root_volume_alias" {
+  name          = "alias/enclaveInstanceRootVolume"
+  target_key_id = aws_kms_key.enclave_instance_root_volume.key_id
 }
 
 resource "aws_kms_key" "enclave_instance_ebs_volume" {
@@ -42,48 +46,64 @@ resource "aws_kms_key_policy" "enclave_instance_ebs_policy" {
   })
 }
 
-# Friendly alias for the KMS key
-resource "aws_kms_alias" "enclave_instance_root_volume_alias" {
-  name          = "alias/enclaveInstanceRootVolume"
-  target_key_id = aws_kms_key.enclave_instance_root_volume.key_id
+resource "aws_kms_key" "secrets_encryption" {
+  description             = "Key for SOPS secrets decryption in Nitro Enclave"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "secrets-encryption-key-policy"
+    Statement = [
+      # --- ADMIN: root can only delete the key, but not use it ---
+      {
+        Sid    = "AllowRootAdminOnly"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action = [
+          "kms:DescribeKey",
+          "kms:List*",
+          "kms:GetKeyPolicy",
+          "kms:GetKeyRotationStatus",
+          "kms:DisableKey",
+          "kms:EnableKey",
+          "kms:EnableKeyRotation",
+          "kms:CreateAlias",
+          // We have to set this otherwise we cannot create a policy
+          // for this key:
+          // AWS operation error KMS: PutKeyPolicy, https response error StatusCode: 400, RequestID: 87120d0e-2a77-40ad-86cd-e1a750f9d6e5, MalformedPolicyDocumentException: The new key policy will not allow you to update the key policy in the future.
+          "kms:PutKeyPolicy",
+
+          // We should be warned by AWS Config about this, or just remove this permission
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ]
+        Resource = "*"
+      },
+
+      # --- USAGE: only EC2 instance role ---
+      {
+        Sid    = "AllowEC2InstanceRoleUseOfKey"
+        Effect = "Allow"
+        Principal = {
+          AWS = "${aws_iam_role.enclave_instance_role.arn}"
+        }
+        Action = [
+          "kms:Encrypt",
+          "kms:DescribeKey",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
-# resource "aws_kms_key" "gocryptfs" {
-#   description             = "Key for gocryptfs on EC2"
-#   deletion_window_in_days = 7
-#   enable_key_rotation     = true
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Id      = "gocryptfs-key-policy"
-#     Statement = [
-#       # --- ADMIN: root can only delete the key, but not use it ---
-#       {
-#         Sid    = "AllowRootAdminOnly"
-#         Effect = "Allow"
-#         Principal = {
-#           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#         }
-#         Action = [
-#           "kms:DescribeKey",
-#           "kms:List*",
-#           "kms:GetKeyPolicy",
-#           "kms:GetKeyRotationStatus",
-#           "kms:DisableKey",
-#           "kms:EnableKey",
-#           // We have to set this otherwise we cannot create a policy
-#           // for this key:
-#           // AWS operation error KMS: PutKeyPolicy, https response error StatusCode: 400, RequestID: 87120d0e-2a77-40ad-86cd-e1a750f9d6e5, MalformedPolicyDocumentException: The new key policy will not allow you to update the key policy in the future.
-#           "kms:PutKeyPolicy",
-#           "kms:ScheduleKeyDeletion",
-#           "kms:CancelKeyDeletion"
-#         ]
-#         Resource = "*"
-#       }
-#     ]
-#   })
-# }
-# resource "aws_kms_alias" "gocryptfs" {
-#   name          = "alias/gocryptfs"
-#   target_key_id = aws_kms_key.gocryptfs.key_id
-# }
+resource "aws_kms_alias" "secrets_encryption" {
+  name          = "alias/secrets_encryption"
+  target_key_id = aws_kms_key.secrets_encryption.key_id
+}
