8d0f7228c47d1bebd3a10942cb14f3ad86767045
Jan Strnádek <jan.strnadek@gmail.com>
2025-10-02 14:44:48 +0200

    fix(agent) Create log group

--- agent/logs.js
+++ agent/logs.js
@@ -1,6 +1,6 @@
-import { CloudWatchLogsClient, PutLogEventsCommand, CreateLogStreamCommand } from "@aws-sdk/client-cloudwatch-logs";
+import { CloudWatchLogsClient, PutLogEventsCommand, CreateLogStreamCommand, CreateLogGroupCommand } from "@aws-sdk/client-cloudwatch-logs";
 
-const logGroupName = "EnclaveLogs";
+const logGroupName = "/ec2/nitro/facesign";
 const logStreamName = "agent-stream";
 const cwLogs = new CloudWatchLogsClient({ region: "eu-west-1" });
 
@@ -8,8 +8,13 @@ let sequenceToken;
 
 async function sendLog(message) {
   try {
-    // Vytvoření log streamu, pokud neexistuje
     if (!sequenceToken) {
+      try {
+        await cwLogs.send(new CreateLogGroupCommand({ logGroupName }));
+      } catch (e) {
+        // Ignore if already exists
+      }
+
       try {
         await cwLogs.send(new CreateLogStreamCommand({
           logGroupName,
@@ -31,7 +36,6 @@ async function sendLog(message) {
 
     const res = await cwLogs.send(new PutLogEventsCommand(params));
     sequenceToken = res.nextSequenceToken;
-
   } catch (err) {
     console.error("CloudWatch Logs error:", err);
   }
