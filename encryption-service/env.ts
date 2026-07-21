26ef38dc5d39109255d7ebfa1d78c08b759ba12c
Jan Strnádek <jan@idos.network>
2026-07-21 10:30:50 +0200

    Apply suggestion from @pkoch
    Co-authored-by: Paulo Koch <pkoch@idos.network>

--- encryption-service/env.ts
+++ encryption-service/env.ts
@@ -1,7 +1,7 @@
 // biome-ignore-all lint/style/noNonNullAssertion: This is ok
 export const GROUP_NAME = "encryption-service";
 export const MONGO_URI = process.env.MONGO_URI!;
-export const FACE_SIGN_ENCRYPTION_COLLECTION = "idOsEncryption";
+export const FACE_SIGN_ENCRYPTION_COLLECTION = "idOSEncryption";
 export const DB_NAME = "encryption";
 export const FLE_KMS_KEY_ID = process.env.AWS_KMS_FLE_ARN!;
 export const FLE_KEY_ALIAS = "encryption-encryption-key";
